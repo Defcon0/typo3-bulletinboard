b040c54103e515f4c7f016e02faf23d16f3fd6b6
dpa <dpa@xima.de>
2024-09-27 12:24:43 +0200

    add type hints for Entry

--- Classes/Domain/Model/Entry.php
+++ Classes/Domain/Model/Entry.php
@@ -2,32 +2,13 @@
 
 namespace WapplerSystems\WsBulletinboard\Domain\Model;
 
-
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
 use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 
-/**
- *
- */
 class Entry extends AbstractEntity
 {
-
-    /**
-     * name
-     *
-     * @var string
-     *
-     */
-    protected $title = '';
-
-
-    /**
-     * message
-     *
-     * @var string
-     */
-    protected $message = '';
-
+    protected string $title = '';
+    protected string $message = '';
 
     /**
      * @var ObjectStorage<\TYPO3\CMS\Extbase\Domain\Model\FileReference>
@@ -35,77 +16,40 @@ class Entry extends AbstractEntity
     #[\TYPO3\CMS\Extbase\Annotation\ORM\Cascade(['value' => 'remove'])] // remove
     protected $images = null;
 
-    /**
-     * tstamp
-     *
-     * @var int
-     */
-    protected $tstamp;
+    protected int $tstamp;
+    protected bool $hidden;
+    protected ?FrontendUser $feUser = null;
 
-    /**
-     * hidden
-     * @var bool
-     */
-    protected $hidden;
-
-
-    /**
-     * @var ?FrontendUser
-     */
-    protected $feUser = null;
-
-    /**
-     * @return string
-     */
     public function getTitle(): string
     {
         return $this->title;
     }
 
-    /**
-     * @return string
-     */
     public function getMessage(): string
     {
         return $this->message;
     }
 
-    /**
-     * @return int
-     */
     public function getTstamp(): int
     {
         return $this->tstamp;
     }
 
-    /**
-     * @return bool
-     */
     public function isHidden(): bool
     {
         return $this->hidden;
     }
 
-    /**
-     * @return FrontendUser|null
-     */
     public function getFeUser(): ?FrontendUser
     {
         return $this->feUser;
     }
 
-    /**
-     * @return ObjectStorage
-     */
     public function getImages(): ObjectStorage
     {
         return $this->images;
     }
 
-    /**
-     * @param bool $hidden
-     * @return void
-     */
     public function setHidden(bool $hidden): void
     {
         $this->hidden = $hidden;
